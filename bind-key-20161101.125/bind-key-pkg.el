547e2a9ce877f1a17d3d9c482c555a5792d3383f
taihangg <taihangg@qq.com>
2016-12-27 18:48:26 +0800

    update packages

--- bind-key-20161101.125/bind-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bind-key" "20161101.125" "A simple way to manage personal keybindings" 'nil 'url: "https://github.com/jwiegley/use-package" :keywords '(" keys keybinding config dotemacs"))
---
+++ sicp-20161219.542/sicp-pkg.el
@@ -0,0 +1,4 @@
+(define-package "sicp" "20161219.542" "Structure and Interpretation of Computer Programs in info format" 'nil :url "https://mitpress.mit.edu/sicp")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
