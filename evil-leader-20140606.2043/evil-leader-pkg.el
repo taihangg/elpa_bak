cf4bf3c161b25f687528f8c74fdd199b657ca5f7
taihangg <taihangg@qq.com>
2016-12-27 16:49:56 +0800

    update

--- evil-leader-20140606.2043/evil-leader-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-leader" "20140606.2043" "let there be <leader>" '((evil "0")) 'url: "https://github.com/cofi/evil-leader" :keywords '(" evil vim-emulation leader"))
--- mwe-log-commands-20100703.2041/mwe-log-commands-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mwe-log-commands" "20100703.2041" "log keyboard commands to buffer" 'nil 'url: "https://github.com/emacsmirror/mwe-log-commands" :keywords '(" help"))
