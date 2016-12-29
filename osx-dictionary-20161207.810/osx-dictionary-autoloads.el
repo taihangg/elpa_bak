0f4becfd8d5f37ca276c763e1ce824b8a4f76398
taihangg <taihangg@qq.com>
2016-12-29 15:44:55 +0800

    add *-autoloads.el

---
+++ osx-dictionary-20161207.810/osx-dictionary-autoloads.el
@@ -0,0 +1,37 @@
+;;; osx-dictionary-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "osx-dictionary" "osx-dictionary.el" (22626
+;;;;;;  17413 169810 900000))
+;;; Generated autoloads from osx-dictionary.el
+
+(autoload 'osx-dictionary-search-input "osx-dictionary" "\
+Search input word and display result with buffer.
+
+\(fn)" t nil)
+
+(autoload 'osx-dictionary-search-pointer "osx-dictionary" "\
+Search word around and display result with buffer.
+
+\(fn)" t nil)
+
+(autoload 'osx-dictionary-get-all-dictionaries "osx-dictionary" "\
+Get all dictionaries as a list.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("osx-dictionary-pkg.el") (22626 17413
+;;;;;;  247815 300000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; osx-dictionary-autoloads.el ends here
---
+++ xterm-color-20161228.736/xterm-color-autoloads.el
@@ -0,0 +1,16 @@
+;;; xterm-color-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("xterm-color.el") (22628 36703 103378
+;;;;;;  100000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; xterm-color-autoloads.el ends here
